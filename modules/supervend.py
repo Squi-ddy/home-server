9074c0c232be6913055622a282ee2de15e69380d
SquiddyPoos <liuwenkai2017@gmail.com>
2022-02-01 01:23:43 +0800

    product api updated

--- modules/supervend.py
+++ modules/supervend.py
@@ -20,27 +20,36 @@ async def check_password(name):
             return None
 
 def init(app):
-    @app.route('/get-products', subdomain = subdomain)
+    @app.route('/products/', subdomain = subdomain)
     async def get_products():
         result = []
         async with (await get_pool(db_name)).connection() as conn:
             async with conn.cursor() as acurs:
                 await acurs.execute("SELECT * FROM products")
                 async for record in acurs:
-                    dict_record = {}
-                    dict_record["product_id"] = record[0]
-                    dict_record["name"] = record[1]
-                    dict_record["desc"] = record[2]
-                    dict_record["company"] = record[3]
-                    dict_record["price"] = record[4]
-                    dict_record["temp"] = record[5]
-                    dict_record["size"] = record[6]
-                    dict_record["country"] = record[7]
-                    dict_record["expiry"] = [record[8].day, record[8].month, record[8].year]
-                    dict_record["stock"] = record[9]
-                    dict_record["images"] = record[10]
-                    result.append(dict_record)
-        return jsonify(result)
+                    result.append({"id": record[0], "name": record[1], "description": record[2]})
+        return jsonify({"products": result})
+
+    @app.route('/products/<product_id>/', subdomain = subdomain)
+    async def get_product_by_id(product_id):
+        async with (await get_pool(db_name)).connection() as conn:
+            async with conn.cursor() as acurs:
+                await acurs.execute("SELECT * FROM products WHERE product_id = %s", (str(product_id),))
+                if (acurs.rowcount < 1): return ("No such product", 404)
+                record = await acurs.fetchone()
+                dict_record = {}
+                dict_record["id"] = record[0]
+                dict_record["name"] = record[1]
+                dict_record["description"] = record[2]
+                dict_record["company"] = record[3]
+                dict_record["price"] = record[4]
+                dict_record["temp"] = record[5]
+                dict_record["size"] = record[6]
+                dict_record["country"] = record[7]
+                dict_record["expiry"] = [record[8].day, record[8].month, record[8].year]
+                dict_record["stock"] = record[9]
+                dict_record["images"] = record[10]
+        return jsonify(dict_record)
 
     @app.route('/users/<username>/', subdomain = subdomain, methods=["GET", "PUT", "POST", "DELETE"])
     async def user_action(username):
